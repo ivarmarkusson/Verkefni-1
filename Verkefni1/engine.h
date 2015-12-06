8dc44379fdb9d8d76ce8decb49e18322807288b5
ivarmarkusson <ivarmar15@ru.is>
2015-12-06 23:45:49 +0000

    bæti við hugmyndum um föll í engine klasanum

--- Verkefni1/engine.h
+++ Verkefni1/engine.h
@@ -15,7 +15,29 @@ public:
     Engine();
 
     bool sqlConnect();
+
+    void sqlAddScientist();               //Á eftir að útfæra <-
     void sqlAddComputer();
+
+    //Vantar remove Scientist og Computer föll
+    //Heyrði að það væri gott að láta föllin bara fela upplýsingarnar
+    //Það er víst einhver skipun í SQL sem gerir það :)
+
+    void sortSientists();               //Á eftir að útfæra <-
+    //fallið á einungis að endurraða
+    //Scientists vectornum í data
+    //áður en hann er prentaður út
+    void sortComputers();               //Á eftir að útfæra <-
+    //fallið á einungis að endurraða
+    //Computers vectornum í data
+    //áður en hann er prentaður út
+
+    vector<Scientist> searchSientists(string searchtext);               //Á eftir að útfæra <-
+    //fallið leitar í frægireinstak af einhverju input-i(searchtext)
+    //Og skilar vectori sem inniheldur alla einstaklingana sem fundust
+    vector<Computer> searchComputers(string searchtext);               //Á eftir að útfæra <-
+    //fallið leitar í Computers af einhverju input-i(searchtext)
+    //Og skilar vectori sem inniheldur allar tölvur sem fundust
 
     //QSqlDatabase startDatabase();
     vector<Scientist> readAscSciDatabase();
